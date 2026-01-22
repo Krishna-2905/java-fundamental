920637c50e205d098658519ccaa29872c8024f0e
Krishna-2905 <krishnachinthoju04@gmail.com>
2026-01-22 09:59:54 +0530

    git init

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,48 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public   static void main(String[] args) {
+        byte rating, classStrength, age, charging, wings;
+        rating = 5;
+        classStrength = 67;
+        age = 22;
+        charging = 56;
+        wings = 2;
+        System.out.println("rating = " + rating);
+        System.out.println("classStrength = " + classStrength);
+        System.out.println("age = " + age);
+        System.out.println("charging = " + charging);
+        System.out.println("wings = " + wings);
+        short year, population, boilingPoint,birthYear;
+        year = 1970;
+        population = 20000;
+        boilingPoint = 285;
+        birthYear = 1980;
+        System.out.println();
+        System.out.println("year = " + year);
+        System.out.println("population = " + population);
+        System.out.println("boilingPoint = " + boilingPoint);
+        System.out.println("birthYear = " + birthYear);
+        System.out.println();
+        int dailyMetroPassengers, appDownloads, votesCount, cityPopulation;
+        dailyMetroPassengers =4500000;
+        appDownloads = 850000000;
+        votesCount = 1000000;
+        cityPopulation = 1200000;
+        System.out.println("dailyMetroPassengers = " + dailyMetroPassengers);
+        System.out.println("appDownloads = " + appDownloads);
+        System.out.println("votesCount = " + votesCount);
+        System.out.println("cityPopulation = " + cityPopulation);
+        System.out.println();
+        long andhaarnumber ,mobileNumber, bankAccountNumber, electricityBillNumber;
+        andhaarnumber = 223300035679876L;
+        mobileNumber = 8074049939L;
+        bankAccountNumber = 200243456675500L;
+        electricityBillNumber = 20000L;
+        System.out.println("andhaarnumber = " + andhaarnumber);
+        System.out.println("mobileNumber = " + mobileNumber);
+        System.out.println("bankAccountNumber = " + bankAccountNumber);
+        System.out.println("electricityBillNumber = " + electricityBillNumber);
+
+    }
+}
